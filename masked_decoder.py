1d17aece67f6fbfbc7f4895573a654cbf9bef6f7
aahluwalia30 <aahluwalia30@gatech.edu>
2021-11-20 13:16:40 -0500

    Started work on transformer decoder

---
+++ masked_decoder.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class MaskedTransformerDecoder(nn.Module):
+    def __init__(self):
+        """
+        Initializing necessary layers for masked decoder
+        """
+        pass
+
+    def forward(self):
+        """
+        Defining forward pass for transformer decoder
+        """
+        pass
