7aa439dc7cb9cba1989d5d10437f15a07f6f462b
Anshul Ahluwalia <anshul.cgm@gmail.com>
2021-11-20 18:09:20 -0500

    Setup boilerplate for masked transformer decoder

--- masked_decoder.py
+++ masked_decoder.py
@@ -1,17 +1,63 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from torch.nn import MultiheadAttention, Linear, Dropout, LayerNorm, ReLU, Embedding
 
+class TransformerLayer(nn.Module)
+    def __init__(self, d_model: int = 256, nhead: int = 3, dim_feedforward: int = 512, dropout: float = 0.2):
+        """
+        Initializing necessary layers for a transformer layer
+        """
+        self.d_model = d_model
+        self.nhead = nhead
+        self.dim_feedforward = dim_feedforward
+        self.dropout = dropout
 
-class MaskedTransformerDecoder(nn.Module):
-    def __init__(self):
-        """
-        Initializing necessary layers for masked decoder
-        """
-        pass
+        self.self_attn = MultiheadAttention(d_model, nhead)
+        
+        self.linear1 = Linear(d_model, dim_feedforward)
+        self.linear2 = Linear(dim_feedforward, d_model)
 
-    def forward(self):
+        self.norm1 = LayerNorm(d_model)
+        self.norm2 = LayerNorm(d_model)
+
+        self.activation = ReLU()
+        self.dropout_layer = Dropout(dropout)
+
+    def forward(self, input, mask = None):
         """
-        Defining forward pass for transformer decoder
+        Defining forward pass for transformer layer
+        Args:
+            input: A tensor of shape [batch_size, num_patches, embed_dim]
+            mask: A mask for self-attention
+        Returns:
+            output: A tensor of shape [batch_size, num_patches, embed_dim]
         """
-        pass
+        first_block = self.self_attn(self.norm1(input), mask) + input
+        norm_first_block = self.norm2(first_block)
+        output = self.linear1(norm_first_block)
+        output = self.activation(output)
+        output = self.dropout(output)
+        output = self.linear2(output)
+        output = self.dropout(output)
+        output += first_block
+        return output
+
+class MaskedTransformer(nn.Module):
+    def __init__(self, d_model: int = 256, nhead: int = 3, dim_feedforward: int = 512, dropout: float = 0.2, patch_size: int = 8, num_layers: int = 6, num_channels: int = 64, num_classes: int = 38):
+        self.d_model = d_model
+        self.nhead = nhead
+        self.dim_feedforward = dim_feedforward
+        self.dropout = dropout
+        self.patch_size = patch_size
+        self.num_layers = num_layers
+        self.num_channels = num_channels
+        self.num_classes = num_classes
+
+        self.linear_embedder = Linear(patch_size * patch_size * num_channels, d_model)
+        self.class_embedder = Embedding(num_classes, d_model)
+
+        self.positional_embedder = nn.Linear(2, d_model)
+
+        self.transformer_layers = nn.ModuleList([TransformerLayer(d_model, nhead, dim_feedforward, dropout) for _ in range(num_layers)])
+        self.norm = LayerNorm(d_model)
