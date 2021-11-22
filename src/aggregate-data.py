f59d3a430ddc986cf504bd9fb8fa64e5a35cb5ce
Adi <adi.singh901@gmail.com>
2021-11-22 10:41:49 -0500

    Added Data Aggregation Script.

--- src/aggregate-data.py
+++ src/aggregate-data.py
@@ -1,12 +1,14 @@
-"""Aggregates all of the training data in a2d2 directory and saves to disk."""
+"""Aggregates all of the training data in a2d2 directory and saves to disk. Saves road scene data 
+and labels at spectr/a2d2_semantic_dataset_subset/data/."""
 
 import os
 from pathlib import Path
 
-import cv2
 import numpy as np
+import torch
+import torchvision
 
-DATA_ROOT = "./../a2d2_semantic_dataset_subset"
+DATA_ROOT = Path(__file__).parent.parent.resolve() / "a2d2_semantic_dataset_subset"
 
 print('Scanning for all images in A2D2 dataset directory...')
 # Collect all file paths under the a2d2 directory and then filter for only .png images (features + labels).
@@ -38,33 +40,28 @@ numDataPairs = int(len(imagePaths) / 2)
 
 print(f'Found {numPNGFiles} .png files total.')
 print(f'Found {numDataPairs} image pairs in dataset.')
+resize = torchvision.transforms.Resize((304, 480))
 
 for idx in range(0, len(imagePaths), 2):
+    if (idx % 100 == 0):
+        print(f'Loaded {idx}/{numDataPairs} images.')
+
     labelFilePath = imagePaths[idx]
     cameraFilePath = imagePaths[idx+1]
 
-    roadImage = cv2.imread(cameraFilePath)
-    roadImage = cv2.resize(roadImage, (480, 304), interpolation=cv2.INTER_AREA)
-    roadImage = cv2.cvtColor(roadImage, cv2.COLOR_BGR2RGB)
-    roadImage = np.transpose(roadImage, (2, 0, 1))
+    roadImage = resize(torchvision.io.read_image(cameraFilePath))
     road_scene_images.append(roadImage)
 
-    roadLabel = cv2.imread(labelFilePath)
-    roadLabel = cv2.resize(roadLabel, (480, 304), interpolation=cv2.INTER_AREA)
-    roadLabel = cv2.cvtColor(roadLabel, cv2.COLOR_BGR2RGB)
-    roadLabel = np.transpose(roadLabel, (2, 0, 1))
+    roadLabel = resize(torchvision.io.read_image(labelFilePath))
     road_scene_labels.append(roadLabel)
 
-    if (idx % 100 == 0):
-        print(f'Loaded {idx+1}/{numDataPairs} images.')
-
-road_scene_images = np.array(road_scene_images)
-road_scene_labels = np.array(road_scene_labels)
+road_scene_images = torch.stack((road_scene_images))
+road_scene_labels = torch.stack((road_scene_labels))
 
 print(f'Tensor shape of road scene images: {road_scene_images.shape}')
 print(f'Tensor shape of road scene labels: {road_scene_labels.shape}')
 
-np.save('./../a2d2_semantic_dataset_subset/data/road_scene_images', road_scene_images)
-np.save('./../a2d2_semantic_dataset_subset/data/road_scene_labels', road_scene_labels)
+torch.save(road_scene_images, DATA_ROOT / "data/road_scene_images.pt")
+torch.save(road_scene_labels, DATA_ROOT / "data/road_scene_labels.pt")
 
 print("Finished aggregating all A2D2 Road Scene Data.  :)")
