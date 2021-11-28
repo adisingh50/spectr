80c99949cc96cb22f1084a2a19c012f653c76ffc
Aditya Singh <adi.singh901@gmail.com>
2021-11-27 19:21:23 -0500

    need to test with Anshul's permissions

--- spectr/misc/cityscapes_preprocess.py
+++ spectr/misc/cityscapes_preprocess.py
@@ -19,12 +19,12 @@ testLabelFilePaths = glob.glob("/srv/datasets/cityscapes/gtFine/test/**/*_color.
 resize = Resize((256, 512), interpolation=InterpolationMode.NEAREST)
 
 # Loop through all image file paths. Modify and save in new dir.
-for filePath in trainImageFilePaths:
-    image = resize(read_image(filePath))
-    image = image / 255.0
-    fileName = filePath.split("/")[-1].replace(".png", "")
-    torch.save(image, f"/coc/scratch/aahluwalia30/cityscapes-processed/leftImg8bit/train/{fileName}.pt")
-print("Completed: Training Images Preprocessed.")
+# for filePath in trainImageFilePaths:
+#     image = resize(read_image(filePath))
+#     image = image / 255.0
+#     fileName = filePath.split("/")[-1].replace(".png", "")
+#     torch.save(image, f"/coc/scratch/aahluwalia30/cityscapes-processed/leftImg8bit/train/{fileName}.pt")
+# print("Completed: Training Images Preprocessed.")
 
 for filePath in valImageFilePaths:
     image = resize(read_image(filePath))
@@ -34,28 +34,28 @@ for filePath in valImageFilePaths:
 print("Completed: Validation Images Preprocessed.")
 
 
-for filePath in testImageFilePaths:
-    image = resize(read_image(filePath))
-    image = image / 255.0
-    fileName = filePath.split("/")[-1].replace(".png", "")
-    torch.save(image, f"/coc/scratch/aahluwalia30/cityscapes-processed/leftImg8bit/test/{fileName}.pt")
-print("Completed: Testing Images Preprocessed.")
+# for filePath in testImageFilePaths:
+#     image = resize(read_image(filePath))
+#     image = image / 255.0
+#     fileName = filePath.split("/")[-1].replace(".png", "")
+#     torch.save(image, f"/coc/scratch/aahluwalia30/cityscapes-processed/leftImg8bit/test/{fileName}.pt")
+# print("Completed: Testing Images Preprocessed.")
 
 
 # Loop through all label file paths. Modify and save in new dir.
-for filePath in trainLabelFilePaths:
-    label = resize(read_image(filePath))
-    label = label[:-1, :, :]
+# for filePath in trainLabelFilePaths:
+#     label = resize(read_image(filePath))
+#     label = label[:-1, :, :]
 
-    # Generate 2d tensor of classIds from 3d semantically segmented label image.
-    label_classIds = torch.zeros(label.shape[1], label.shape[2])
-    for row in range(label.shape[1]):
-        for col in range(label.shape[2]):
-            label_classIds[row, col] = get_classId_from_rgb(label[:, row, col])
+#     # Generate 2d tensor of classIds from 3d semantically segmented label image.
+#     label_classIds = torch.zeros(label.shape[1], label.shape[2])
+#     for row in range(label.shape[1]):
+#         for col in range(label.shape[2]):
+#             label_classIds[row, col] = get_classId_from_rgb(label[:, row, col])
 
-    fileName = filePath.split("/")[-1].replace(".png", "")
-    torch.save(label_classIds, f"/coc/scratch/aahluwalia30/cityscapes-processed/gtFine/train/{fileName}.pt")
-print("Completed: Training Labels Preprocessed.")
+#     fileName = filePath.split("/")[-1].replace(".png", "")
+#     torch.save(label_classIds, f"/coc/scratch/aahluwalia30/cityscapes-processed/gtFine/train/{fileName}.pt")
+# print("Completed: Training Labels Preprocessed.")
 
 
 for filePath in valLabelFilePaths:
@@ -73,16 +73,16 @@ for filePath in valLabelFilePaths:
 print("Completed: Validation Labels Preprocessed.")
 
 
-for filePath in testLabelFilePaths:
-    label = resize(read_image(filePath))
-    label = label[:-1, :, :]
+# for filePath in testLabelFilePaths:
+#     label = resize(read_image(filePath))
+#     label = label[:-1, :, :]
 
-    # Generate 2d tensor of classIds from 3d semantically segmented label image.
-    label_classIds = torch.zeros(label.shape[1], label.shape[2])
-    for row in range(label.shape[1]):
-        for col in range(label.shape[2]):
-            label_classIds[row, col] = get_classId_from_rgb(label[:, row, col])
+#     # Generate 2d tensor of classIds from 3d semantically segmented label image.
+#     label_classIds = torch.zeros(label.shape[1], label.shape[2])
+#     for row in range(label.shape[1]):
+#         for col in range(label.shape[2]):
+#             label_classIds[row, col] = get_classId_from_rgb(label[:, row, col])
 
-    fileName = filePath.split("/")[-1].replace(".png", "")
-    torch.save(label_classIds, f"/coc/scratch/aahluwalia30/cityscapes-processed/gtFine/test/{fileName}.pt")
-print("Completed: Testing Labels Preprocessed.")
+#     fileName = filePath.split("/")[-1].replace(".png", "")
+#     torch.save(label_classIds, f"/coc/scratch/aahluwalia30/cityscapes-processed/gtFine/test/{fileName}.pt")
+# print("Completed: Testing Labels Preprocessed.")
